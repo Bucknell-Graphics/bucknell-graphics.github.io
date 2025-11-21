d89cffbca9db811e813d28f77b8a887f16990ebe
Sing Chun Lee <leesingchun@gmail.com>
2025-11-20 20:52:54 -0500

    Added new page for SP26 course

--- style/theme.js
+++ style/theme.js
@@ -5,9 +5,49 @@ function setTheme(unset=true) {
     document.body.classList.toggle(prevStr);
   }
   document.body.classList.toggle(classStr);
+  let year = document.getElementById("year-select");
+  var useBucknell = true;
+  if (year !== undefined && year.value == "SP26") useBucknell = false;
   let logo = document.getElementById("logo");
-  if (logo !== undefined) logo.src = getComputedStyle(document.body).getPropertyValue('--bu-logo-path');
+  if (logo !== undefined) {
+    if (useBucknell) logo.src = getComputedStyle(document.body).getPropertyValue('--bu-logo-path');
+    else logo.src = getComputedStyle(document.body).getPropertyValue('--uop-logo-path');
+  }
 }
+
+function loadYearTable(year) {
+  fetch(`${year}/table.html`)
+    .then(response => response.text())
+    .then(html => {
+      document.getElementById("demotable").innerHTML = html;
+    });
+  var useBucknell = true;
+  if (year == "SP26") useBucknell = false;
+  let logo = document.getElementById("logo");
+  if (logo !== undefined) {
+    if (useBucknell) logo.src = getComputedStyle(document.body).getPropertyValue('--bu-logo-path');
+    else logo.src = getComputedStyle(document.body).getPropertyValue('--uop-logo-path');
+  }
+  let title = document.getElementById("title");
+  if (title !== undefined) {
+    if (useBucknell) title.textContent = "CSCI 379 Computer Graphics Wizard Academy Demos";
+    else title.textContent = "COMP 193 Computer Graphics Wizard Academy Demos";
+  }
+  let copyright = document.getElementById("copyright");
+  if (copyright !== undefined) copyright.textContent = `Copyright © 20${year.slice(-2)} Sing Chun Lee @ Bucknell University. All rights reserved.`;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const selector = document.getElementById("year-select");
+
+  // Load initial year from the select box
+  loadYearTable(selector.value);
+
+  selector.addEventListener("change", (event) => {
+    loadYearTable(event.target.value);
+  });
+});
+
 window.onload = function() {
   // Create these two tags
   // <input type="checkbox" id="theme" style="display: none" />
